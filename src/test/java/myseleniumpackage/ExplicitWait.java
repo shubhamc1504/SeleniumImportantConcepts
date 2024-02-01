2d9f06d2917ccab2c5483a047db25449cbbd95c6
shubhamc1504 <shubhamc1504@gmail.com>
2024-02-01 22:29:21 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/test/java/myseleniumpackage/ExplicitWait.java
@@ -0,0 +1,32 @@
+package myseleniumpackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class ExplicitWait {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver(); //Instantiate the chromedriver
+        driver.get("https://www.google.com/"); //URL
+        driver.manage().window().maximize(); //maximize the web page
+        driver.findElement(By.name("q")).sendKeys("Selenium"); //Selenium passed in search box section
+        driver.findElement(By.name("q")).sendKeys(Keys.RETURN); //Press ENTER Button
+
+        By elelocator = By.xpath("//a[normalize-space()='WebDriver']");
+        waitForElementPresent(driver, elelocator, 10).click(); //returning the webelement on which we click on
+        driver.quit();
+    }
+
+    public static WebElement waitForElementPresent(WebDriver driver, By locator, int timeout) {
+        WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(timeout));
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+        return driver.findElement(locator); //returning the web element
+    }
+}
