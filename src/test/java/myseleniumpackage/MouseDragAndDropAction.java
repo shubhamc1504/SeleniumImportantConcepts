912876b4eebe2bbcda2d97b462dfee47c698abf4
shubhamc1504 <shubhamc1504@gmail.com>
2024-02-04 15:50:55 +0530

    Commit on 4th Feb 2024

--- src/test/java/myseleniumpackage/MouseDragAndDropAction.java
+++ src/test/java/myseleniumpackage/MouseDragAndDropAction.java
@@ -1,6 +1,5 @@
 package myseleniumpackage;
 
-import org.checkerframework.checker.units.qual.A;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
