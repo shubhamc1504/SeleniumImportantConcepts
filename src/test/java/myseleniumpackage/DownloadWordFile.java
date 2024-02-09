2e73ae248bddd5aebbbbc7c567d6d125d3f7fbe0
shubhamc1504 <shubhamc1504@gmail.com>
2024-02-09 21:14:59 +0530

    Commit on 8th Feb 2024

---
+++ src/test/java/myseleniumpackage/DownloadWordFile.java
@@ -0,0 +1,32 @@
+package myseleniumpackage;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+
+public class DownloadWordFile {
+    public static void main(String[] args)
+    {
+        String location= System.getProperty("user.dir")+"\\Downloads\\";
+
+        //Chrome:-
+        HashMap preferences=new HashMap();
+        preferences.put("download.default_directory", location);
+
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", preferences);
+
+        System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver(options);
+        driver.get("https://file-examples.com/index.php/sample-documents-download/sample-doc-download/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.xpath("//tbody/tr[1]/td[5]/a[1]")).click();
+
+    }
+
+}
